af1fb97fb66b62674f79b737adac29c94a6cc428
Aii如樱如月 <morikawa2021@163.com>
2025-01-07 20:29:16 +0800

    env config

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
+/*
+ * @Author: Aii如樱如月 morikawa2021@163.com
+ * @Date: 2025-01-07 19:51:55
+ * @LastEditors: Aii如樱如月 morikawa2021@163.com
+ * @LastEditTime: 2025-01-07 19:59:25
+ * @FilePath: \AynaLivePlayerOBSInfo\env.d.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 /// <reference types="vite/client" />
+declare module '*.vue'
