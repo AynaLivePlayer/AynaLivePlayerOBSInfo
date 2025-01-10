4f2a1956672d811523bb82b53e082623cff1091a
Aiikisaraki <morikawa@kimisui56.work>
2025-01-10 16:26:36 +0800

    update sandbox test page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,14 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2025-01-08 23:06:16
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2025-01-10 16:22:26
+ * @FilePath: \AynaLivePlayerOBSInfo\src\router\index.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import {createRouter, createWebHistory} from 'vue-router'
 import AboutView from "@/views/AboutView.vue";
+import Sandbox from '@/views/Sandbox.vue';
 import Player1 from "@/views/builtin/player1/Player1.vue";
 import Player2 from "@/views/builtin/player2/Player2.vue";
 import Player2Right from "@/views/builtin/player2/Player2Right.vue";
@@ -59,6 +68,11 @@ const router = createRouter({
             name: 'media_session',
             component: MediaSessionVue,
         },
+        {
+            path: '/sandbox',
+            name: 'sandbox',
+            component: Sandbox,
+        }
     ]
 })
 
---
+++ src/views/user/AiiKisaraki/PlayerPage-Aii.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2025-01-09 14:00:00
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2025-01-09 14:12:30
+ * @FilePath: \AynaLivePlayerOBSInfo\src\views\user\AiiKisaraki\PlayerPage-Aii.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    
+</template>
+
+<style lang="less" scoped>
+
+</style>
