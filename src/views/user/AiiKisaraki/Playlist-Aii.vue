7fec80bda0de5abc021c42676f045a86b2a7ea50
Aii如樱如月 <morikawa2021@163.com>
2025-01-21 14:15:23 +0800

    change proj config

--- src/views/user/AiiKisaraki/Playlist-Aii.vue
+++ src/views/user/AiiKisaraki/Playlist-Aii.vue
@@ -1,11 +1,28 @@
 <script lang="ts" setup>
+import {usePlayInfoStore} from "@/stores/playinfo";
+import {onMounted, watch} from "vue";
+import PlaylistTitle from "@/components/playlist/PlaylistTitle.vue";
+import PlaylistArtist from "@/components/playlist/PlaylistArtist.vue";
+import PlaylistUser from "@/components/playlist/PlaylistUser.vue";
+
+
+const playInfoStore = usePlayInfoStore();
+
+
 
 </script>
 
 <template>
-    
+  <div class="playlist-content m-4 overflow-hidden">
+    <div class="play-item">
+
+    </div>
+  </div>
 </template>
 
 <style lang="less" scoped>
-
+  .playlist-content {
+    width: 100%;
+    height: 100%;
+  }
 </style>
