7381fa05a5913fade770415ef05f9163084f7f39
Aii如樱如月 <morikawa2021@163.com>
2025-02-11 10:27:38 +0800

    fixed the mini lyric text align

--- src/views/user/AiiKisaraki/PlayerPage-Aii.vue
+++ src/views/user/AiiKisaraki/PlayerPage-Aii.vue
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2025-01-09 14:00:00
  * @LastEditors: Aii如樱如月 morikawa2021@163.com
- * @LastEditTime: 2025-01-12 23:13:50
+ * @LastEditTime: 2025-02-11 10:27:08
  * @FilePath: \AynaLivePlayerOBSInfo\src\views\user\AiiKisaraki\PlayerPage-Aii.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -368,6 +368,7 @@ const playlistStatus = computed(() => {
     position: absolute;
     left: 46%;
     color: #0d16a0;
+    text-align: center;
     font-size: 40px;
     transition: all 1s;
     display: flex;
